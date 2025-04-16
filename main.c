7c1d5a95d66889bf8c4aeef102e1cdbda951b08c
Alexis Baude <141053416+alexisbaude@users.noreply.github.com>
2025-04-16 11:32:04 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Fonction d'initialisation
+void init_system() {
+    // Affiche un message de bienvenue
+    printf("Bienvenue sur LNT OS !\n");
+}
+
+// Fonction principale
+int main() {
+    // Initialisation du système
+    init_system();
+
+    // Affiche un message pour dire que le système est prêt
+    printf("Le système est prêt à être utilisé.\n");
+
+    return 0;
+}
